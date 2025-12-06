f06530e7f81f50b620c310e6a96067e10202590f
Aydan <aydanqurbanova2006@gmail.com>
2025-12-06 20:01:37 +0000

    .

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet from 'a' to 'z'
+ *              followed by a new line, using putchar twice.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
