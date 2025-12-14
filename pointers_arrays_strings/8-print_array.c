da90ee8de690d8eee53eec5c6c41ac3131ed994a
agurbanova22777 <agurbanova22777@ada.edu.az>
2025-12-14 22:03:56 +0400

    .

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array of integers.
+ * @a: Pointer to the array of integers.
+ * @n: The number of elements to print.
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		/* If this is not the last element, print the separator */
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
