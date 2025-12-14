edbbc698de36ae2e932ff5d848fb509153900310
agurbanova22777 <agurbanova22777@ada.edu.az>
2025-12-14 20:12:19 +0400

    .

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * @n: A pointer to an integer.
+ * reset_to_98 - Updates the value a pointer points to to 98.
+ * @n: A pointer to an integer that will be updated.
  *
- * Return: void.
+ * Return: void. This function does not return a value.
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
