15c44fc030061d7b4e15bf707f9acd07d6f9b824
agurbanova22777 <agurbanova22777@ada.edu.az>
2025-12-21 21:56:52 +0400

    .

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find the factorial of.
+ *
+ * Return: The factorial of n. If n is lower than 0, returns -1.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
