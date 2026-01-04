2805e28a2fa294fbaa7ec2be2617bdfb8fda3bb7
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-01-04 23:14:10 +0400

    .

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif /* DOG_H */
