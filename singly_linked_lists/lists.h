9be11d3948a1815f762ed4c940e73396ccacfc44
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-03-09 20:51:44 +0400

    .

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -21,6 +21,7 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 int _putchar(char c);
 
 char *strdup(const char *s);
